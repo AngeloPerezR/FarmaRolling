1ee0d9f16c890fc691328e6e9dfbdc6e4566270d
Angelo Perez <angelo.perez@exponentia.tech>
2024-09-08 19:28:09 -0500

    Proyecto FarmaRolling

---
+++ models/usuario.schema.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose')
+
+const UsuarioSchema = new Schema({
+  nombreUsuario:{
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+
+  contrasenia: {
+    type: String,
+    required: true,
+    trim:true
+  },
+
+  rol: {
+    type: String,
+    default: 'usuario',
+    enum:['usuario', 'admin']
+  },
+
+  bloqueado:{
+    type:Boolean,
+    default: false
+  },
+  idCarrito:{
+    type: String
+  },
+  idFavoritos:{
+    type:String
+  },
+})
+
+UsuarioSchema.methods.toJSON = function(){
+  const { contrasenia, __v, ...usuario } = this.toObject()
+  return usuario 
+}
+
+const UsuarioModel = model('user', UsuarioSchema)
+module.exports = UsuarioModel
---
+++ routes/productos.routes.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const { obtenerUnProductoPorIdOTodos, crearProducto, editarProductoPorId, eliminarProductoPorId, agregarImagenProductoPorId, buscarProductoPorTermino, agregarProductoAlCarrito, borrarProductoCarrito, agregarProductoAlFavoritos, borrarProductoFavoritos, mercadoPago } = require('../controllers/productos.controllers')
+const { check } = require('express-validator')
+const auth = require('../middlewares/auth')
+const multer = require('../middlewares/multer')
+const router = express.Router()
+
+/* GET - Obtener*/
+router.get('/', obtenerUnProductoPorIdOTodos)
+router.get('/buscar', buscarProductoPorTermino)
+/* POST - Crear */
+router.post('/', [
+  check('nombre', 'campo NOMBRE vacio').not().isEmpty(),
+  check('precio', 'campo PRECIO vacio').not().isEmpty(),
+  check('descripcion', 'campo DESCRIPCION vacio').not().isEmpty(),
+], auth('admin'), crearProducto)
+
+router.post('/crearPago', mercadoPago)
+
+router.post('/agregarProductoCarrito/:idProducto', auth('usuario'), agregarProductoAlCarrito)
+router.post('/quitarProductoCarrito/:idProducto', auth('usuario'), borrarProductoCarrito)
+
+router.post('/agregarProductoFav/:idProducto', auth('usuario'), agregarProductoAlFavoritos)
+router.post('/quitarProductoFav/:idProducto', auth('usuario'), borrarProductoFavoritos)
+
+
+router.post('/agregarImagen/:idProducto', multer.single('imagen'),agregarImagenProductoPorId)
+
+/* PUT - Editar */
+router.put('/:idProducto',
+  [
+    check('nombre', 'campo NOMBRE vacio').not().isEmpty(),
+    check('precio', 'campo PRECIO vacio').not().isEmpty(),
+    check('descripcion', 'campo DESCRIPCION vacio').not().isEmpty(),
+  ], auth('admin'), editarProductoPorId)
+
+/* DELETE  - Borrar */
+router.delete('/:idProducto', auth('admin'), eliminarProductoPorId)
+
+module.exports = router
